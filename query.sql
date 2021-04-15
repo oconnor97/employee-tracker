03ef4cf2ec7be54bdd961b208f71a44f27ac747f
Michael O'Connor <oconnor97@aol.com>
2021-04-15 11:00:47 -0400

    adding foreign keys so I could access information inside different tables

--- query.sql
+++ query.sql
@@ -15,6 +15,8 @@ id INT NOT NULL AUTO_INCREMENT KEY,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT
+FOREIGN KEY (department_id) REFERENCES department(id)
+
 );
 
 CREATE TABLE employee (
@@ -22,5 +24,9 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
-manager_id INT
+manager_id INT,
+FOREIGN KEY (role_id) REFERENCES role(id),
+FOREIGN KEY (manager_id) REFERENCES employee(id);
+
+
 );
